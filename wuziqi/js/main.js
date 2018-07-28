2b93738d5ec8c9368f24d5362eed2c86023fb5f8
sparta <184966561@qq.com>
2018-07-28 17:34:36 +0800

    更新了电脑最后一子的提示

--- wuziqi/js/main.js
+++ wuziqi/js/main.js
@@ -1,16 +1,18 @@
 
 
-var oCanvas = document.getElementById("wuziqi");
-var context = oCanvas.getContext("2d");
+var oCanvas = document.getElementById("wuziqi");//获取id
+var context = oCanvas.getContext("2d");//获取画布
 var me=true;
 var chessBoard=[];
 var wins=[];
 
 
-var playerWin = [];
-var computerWin = [];
+    var playerWin = [];//玩家赢法
+    var computerWin = [];//电脑赢法
 
-var over=false;
+    var over=true;//结束判定
+
+    //棋盘大小
     for(var i=0;i<19;i++){
         chessBoard[i]=[];
         for(var j=0;j<19;j++){
@@ -24,8 +26,8 @@ var over=false;
        }
    }
 
-
-var count=0;
+    //遍历赢法
+    var count=0;
    for(var i=0;i<19;i++){
        for(var j=0;j<15;j++){
            for(var k=0;k<5;k++){
@@ -40,28 +42,28 @@ var count=0;
             wins[j+k][i][count]=true;
         }
         count++;
-    }    
-}
+     }    
+    }
 for(var i=0;i<15;i++){
     for(var j=0;j<15;j++){
         for(var k=0;k<5;k++){
             wins[i+k][j+k][count]=true;
         }
         count++;
-    }    
-}
+     }    
+    }
    for(var i=0;i<15;i++){
     for(var j=18;j>3;j--){
         for(var k=0;k<5;k++){
             wins[i+k][j-k][count]=true;
-        }
+          }
         count++;
-    }    
-}
+     }    
+    }
 
 
-console.log(count);
-
+    console.log(count);
+//初始化
     for(var i=0;i<count;i++){
         playerWin[i]=0;
         computerWin[i]=0;
@@ -75,9 +77,8 @@ console.log(count);
         context.drawImage(logo,100,100,400,400);
         qipan();
 
-       
-
     }
+    //绘制棋盘
 var qipan=function (){
     for(var i=0; i<19;i++){
         context.moveTo(15+i*30,15);
@@ -88,7 +89,7 @@ var qipan=function (){
         context.stroke();
     } 
 }
-
+//落子
 var oneStep = function(i,j,me){
     context.beginPath();
     context.arc(15+i*30,15+j*30,13,0,2*Math.PI);
@@ -107,17 +108,48 @@ var oneStep = function(i,j,me){
     context.fillStyle=gradient;
     context.fill();
 } 
+//最后落子标记
+function lastStep(i,j){
+    context.fillStyle="#e66767";
+    context.beginPath();
+    context.arc(15+i*30,15+j*30,3,0,2*Math.PI);
+    context.closePath();
+    context.fill();
+}
 
+//清空标记
+function clearStep(i,j){
+    // context.fillStyle="#efefef";
+    context.beginPath();
+    context.arc(15+i*30,15+j*30,5,0,2*Math.PI);
+    context.closePath();
+    var gradient=context.createRadialGradient(15+i*30+2,15+j*30-2,13,15+i*30+2,15+j*30-2,0);
+    gradient.addColorStop(0,"#d1d1d1");
+    gradient.addColorStop(1,"#f9f9f9");
+    context.fillStyle=gradient;
+    context.fill();
+}
 wuziqi.onclick=function(e){
-    if(over){return;}
+    if(!over){return;}
     if(!me){return;}
     var x=e.offsetX;
     var y=e.offsetY;
     var i=Math.floor(x/30);
     var j=Math.floor(y/30);
+    var sp;
     if(chessBoard[i][j]==0){
         oneStep(i,j,me);
+        
         chessBoard[i][j]=1; 
+        sp=true;
+        }else if(chessBoard[i][j]==1){
+            //无法落子
+            sp=false; 
+            
+
+        }else if(chessBoard[i][j]==2){
+            // sp=false;
+            sp=false;
         }
      
         for(var k=0;k<count;k++){
@@ -125,7 +157,7 @@ wuziqi.onclick=function(e){
                 playerWin[k]++;
                 computerWin[k]=6;
                 if(playerWin[k]==5){
-                    over=true;
+                    over=false;
                     setTimeout(function(){
                         window.alert("玩的好呀。");
                     },200);
@@ -134,15 +166,17 @@ wuziqi.onclick=function(e){
                 }
             }
         }
-        if(!over){
+        
+        if(over&&sp){
             me=!me;
+            // lastStep(i,j);
             computerAI();
 
-        }
-
+           }
     }
    
-
+    var lastu=[0];
+    var lastv=[0];
 
 
 var computerAI = function(){
@@ -150,7 +184,8 @@ var computerAI = function(){
     var computerScore=[];
     var max=0;
     var u=0,v=0;
-    for(var i = 0;i<19;i++){
+
+        for(var i = 0;i<19;i++){
         playerScore[i]=[];
         computerScore[i]=[];
         for(var j=0;j<19;j++){
@@ -158,6 +193,7 @@ var computerAI = function(){
             computerScore[i][j]=0;
         }
     }
+    
     for(var i=0;i<19;i++){
         for(var j=0;j<19;j++){
             if(chessBoard[i][j]==0){
@@ -183,6 +219,7 @@ var computerAI = function(){
                         }
                     }
                 }
+               
                 if(playerScore[i][j]>max){
                     max=playerScore[i][j];
                     u=i;
@@ -206,14 +243,21 @@ var computerAI = function(){
             }
         }
     }
+        lastu.push(u);
+        lastv.push(v);
+       console.log(lastu);
+       console.log(lastv);
+   
     oneStep(u,v,false);
+    lastStep(u,v);
+    clearStep(lastu[lastu.length-2],lastv[lastv.length-2]);
     chessBoard[u][v]=2;
       for(var k=0;k<count;k++){
                 if(wins[u][v][k]){
                     playerWin[k]=6;
                     computerWin[k]++;
                     if(computerWin[k]==5){
-                        over=true;
+                        over=false;
                         setTimeout(function(){
                             window.alert("电脑都下不过如何和我同台竞技？");
                         },200);
@@ -224,7 +268,9 @@ var computerAI = function(){
                 }
             
             }
-            if(!over){
+            if(over){
                 me=!me;
+               
+
             }    
 }
